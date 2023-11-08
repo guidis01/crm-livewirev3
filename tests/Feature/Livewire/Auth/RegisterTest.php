3133c641f7a2d47f4f1ac2b54ad81419b38ec240
guilherme silva <guilherme.augusto@globalhitss.com.br>
2023-11-08 14:53:50 -0300

    crm-1: validation rules for our registration form

--- tests/Feature/Livewire/Auth/RegisterTest.php
+++ tests/Feature/Livewire/Auth/RegisterTest.php
@@ -3,6 +3,8 @@
 use App\Livewire\Auth\Register;
 use Livewire\Livewire;
 
+use function Pest\Laravel\{assertDatabaseCount, assertDatabaseHas};
+
 it('should render the component', function () {
     Livewire::test(Register::class)
     ->assertStatus(200);
@@ -24,3 +26,18 @@ it('should be able to register a new user in the system', function () {
 
     $this->assertDatabaseCount('users', 1);
 });
+
+test('validation rules', function ($f) {
+    Livewire::test(Register::class)
+        ->set($f->field, $f->value)
+        ->call('submit')
+        ->assertHasErrors([$f->field => $f->rule]);
+})->with([
+    'name::required'     => (object)['field' => 'name', 'value' => '', 'rule' => 'required'],
+    'name::max:255'      => (object)['field' => 'name', 'value' => str_repeat('*', 256), 'rule' => 'max'],
+    'email::required'    => (object)['field' => 'email', 'value' => '', 'rule' => 'required'],
+    'email::email'       => (object)['field' => 'email', 'value' => 'not-an-email', 'rule' => 'email'],
+    'email::max:255'     => (object)['field' => 'email', 'value' => str_repeat('*' . '@doe.com', 256), 'rule' => 'max'],
+    'email::confirmed'   => (object)['field' => 'email', 'value' => 'john@doe.com', 'rule' => 'confirmed'],
+    'password::required' => (object)['field' => 'password', 'value' => '', 'rule' => 'required'],
+]);
