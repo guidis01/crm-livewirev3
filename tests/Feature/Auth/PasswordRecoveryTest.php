2cf4efc3737d75f605662d62edbbac6af0a51ead
guilherme silva <guilherme.augusto@globalhitss.com.br>
2023-12-11 11:36:59 -0300

    crm-4: testing email property, making sure its required and is a real email

--- tests/Feature/Auth/PasswordRecoveryTest.php
+++ tests/Feature/Auth/PasswordRecoveryTest.php
@@ -28,3 +28,14 @@ it('should be able to request for a password recovery sending a notification to 
     Notification::assertSentTo($user, PasswordRecoveryNotification::class);
 
 });
+
+test('testing email property', function ($value, $rule) {
+    Livewire::test(Recovery::class)
+        ->set('email', $value)
+        ->call('startPasswordRecovery')
+        ->assertHasErrors(['email' => $rule]);
+
+})->with([
+    'required' => ['value' => '', 'rule' => 'required'],
+    'email'    => ['value' => 'any email', 'rule' => 'email'],
+]);
