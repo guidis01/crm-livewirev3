6e1e2e72b175fa5b69dac18f6c6e227ef52de658
guilherme silva <guilherme.augusto@globalhitss.com.br>
2023-12-28 16:19:48 -0300

    crm-17: it should be able to delete a user

---
+++ app/Livewire/Admin/Users/Delete.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Livewire\Admin\Users;
+
+use App\Models\User;
+use Livewire\Component;
+
+class Delete extends Component
+{
+    public User $user;
+
+    public function render()
+    {
+        return view('livewire.admin.users.delete');
+    }
+
+    public function destroy(): void
+    {
+        $this->user->delete();
+
+        $this->dispatch('user::deleted');
+    }
+}
