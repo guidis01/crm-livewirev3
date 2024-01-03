7575194e4b4e0e75d72fbf2d5c228a7b68360be7
guilherme silva <guilherme.augusto@globalhitss.com.br>
2024-01-03 13:44:21 -0300

    crm-8: making sure we are logging with the given user

---
+++ app/Http/Middleware/HandleImpersonation.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class HandleImpersonation
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Closure(Request): (Response) $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if ($id = session('impersonate')) {
+            auth()->onceUsingId($id);
+        }
+
+        return $next($request);
+    }
+}
