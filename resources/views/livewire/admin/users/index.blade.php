47df03a9fdb67c66676326a61201998c359a957c
guilherme silva <guilherme.augusto@globalhitss.com.br>
2024-01-03 09:46:09 -0300

    crm-19: fixing neon error

--- resources/views/livewire/admin/users/index.blade.php
+++ resources/views/livewire/admin/users/index.blade.php
@@ -64,8 +64,12 @@
                     />
                 @endif
             @else
-                <x-button icon="o-arrow-path-rounded-square" wire:click="restore({{ $user->id }})" spinner
-                          class="btn-sm btn-success btn-ghost"/>
+                <x-button
+                    icon="o-arrow-path-rounded-square"
+                    wire:click="restore({{ $user->id }})"
+                    spinner
+                    class="btn-sm btn-success btn-ghost"
+                />
             @endunless
         @endcan
         @endscope
@@ -74,4 +78,5 @@
     {{ $this->users->links(data: ['scrollTo' => false]) }}
 
     <livewire:admin.users.delete/>
+    <livewire:admin.users.restore/>
 </div>
