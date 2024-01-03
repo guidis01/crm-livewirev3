34870eca78ba38837fde20e6519d4e7476513201
guilherme silva <guilherme.augusto@globalhitss.com.br>
2024-01-03 15:49:40 -0300

    crm-9: it should be able to list all the users

---
+++ app/Livewire/Dev/Login.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Livewire\Dev;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\View\View;
+use Livewire\Attributes\Computed;
+use Livewire\Component;
+
+class Login extends Component
+{
+    public ?int $selectedUser = null;
+
+    public function render(): View
+    {
+        return view('livewire.dev.login');
+    }
+
+    #[Computed]
+    public function users(): Collection
+    {
+        return User::all();
+    }
+}
--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -8,22 +8,16 @@ use Illuminate\Database\Seeder;
 
 class UsersSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
         User::factory()
-             ->withPermission(Can::BE_AN_ADMIN)
-             ->create([
-                 'name'  => 'Admin do CRM',
-                 'email' => 'admin@admin.com',
-             ]);
+            ->withPermission(Can::BE_AN_ADMIN)
+            ->create([
+                'name'  => 'Admin do CRM',
+                'email' => 'admin@crm.com',
+            ]);
 
         User::factory()->count(50)->create();
-        User::factory()->count(50)->deleted()->create();
-
-        $user = User::find(3);
-        $user->givePermissionTo(Can::BE_AN_ADMIN);
+        User::factory()->count(10)->deleted()->create();
     }
 }
