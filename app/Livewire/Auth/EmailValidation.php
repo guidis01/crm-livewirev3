6d013932fecc2f1a9df22f8923af3e14e945fd77
guilherme silva <guilherme.augusto@globalhitss.com.br>
2024-01-04 11:04:36 -0300

    crm-21: creating our email validation page

--- app/Livewire/Auth/EmailValidation.php
+++ app/Livewire/Auth/EmailValidation.php
@@ -6,12 +6,16 @@ use App\Events\SendNewCode;
 use App\Notifications\WelcomeNotification;
 use App\Providers\RouteServiceProvider;
 use Illuminate\View\View;
+use Livewire\Attributes\Layout;
 use Livewire\Component;
 
 class EmailValidation extends Component
 {
     public ?string $code = null;
 
+    public ?string $sendNewCodeMessage = null;
+
+    #[Layout('components.layouts.guest')]
     public function render(): View
     {
         return view('livewire.auth.email-validation');
@@ -19,6 +23,7 @@ class EmailValidation extends Component
 
     public function handle(): void
     {
+        $this->reset('sendNewCodeMessage');
         $this->validate([
             'code' => function (string $attribute, mixed $value, \Closure $fail) {
                 if ($value != auth()->user()->validation_code) {
@@ -42,5 +47,8 @@ class EmailValidation extends Component
         SendNewCode::dispatch(
             auth()->user()
         );
+
+        $this->sendNewCodeMessage = 'Code was sent to you. Check your mailbox';
+
     }
 }
